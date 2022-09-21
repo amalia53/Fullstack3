486c01c92863d3475af77f1b1e18625bdc05e34e
Amalia <amalia@pop-os.localdomain>
2022-09-21 16:57:09 +0200

    3.19

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const express = require("express")
 const app = express()
 
 const morgan = require("morgan")
-const { collection } = require("./models/person")
 const Person = require("./models/person")
 
 morgan.token("body", function get_body(req) {
@@ -48,10 +47,10 @@ app.delete("/api/persons/:id", (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const body = request.body
 
-    if (body.name === "" || body.number === "") {
+    if (body.name === undefined || body.number === undefined) {
         return response.status(400).json({
             error: "name and number missing"
         })
@@ -62,20 +61,21 @@ app.post("/api/persons", (request, response) => {
         number: body.number
     })
 
-    newPerson.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+    newPerson.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.put("/api/persons/:id", (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
 
-    const updated = {
-        name: body.name,
-        number: body.number
-    }
-
-    Person.findByIdAndUpdate(request.params.id, updated, { new: true })
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: "query" }
+    )
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -84,8 +84,11 @@ app.put("/api/persons/:id", (request, response, next) => {
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-    if (error.name === 'CastError') {
+    if (error.name === "CastError") {
         return response.status(400).send({ error: 'malformatted id' })
+    }
+    else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
